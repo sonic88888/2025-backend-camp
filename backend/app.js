2232603e61e862e1645582268a88155fde70e7f0
sonic88888 <gwsonicms@outlook.com>
2026-02-01 20:37:31 +0800

    Part 1：初階任務

--- backend/app.js
+++ backend/app.js
@@ -28,8 +28,11 @@ app.use(pinoHttp({
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/healthcheck', (req, res) => {
-  res.status(200)
-  res.send('OK')
+  res.status(200).json({
+  status: 'success',
+  message: 'OK'
+})
+Part
 })
 app.use('/api/credit-package', creditPackageRouter)
 app.use('/api/coaches/skill', skillRouter)
